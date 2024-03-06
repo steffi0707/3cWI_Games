6b3f5604ede913f51193917e69969bada53e9b8b
Steffi <stefanie.trautz@gmail.com>
2024-03-06 09:27:01 +0100

    worked on Rectangles, Circles ans Ovals

--- src/at/steffi/games/firstgame/Oval.java
+++ src/at/steffi/games/firstgame/Oval.java
@@ -1,41 +1,38 @@
 package at.steffi.games.firstgame;
 
 import org.newdawn.slick.*;
-import org.newdawn.slick.tests.AnimationTest;
 
-public class Rectangles extends BasicGame {
+public class Oval extends BasicGame {
     private float x;
     private float y;
     private float speed;
 
-
-    public Rectangles(String title) {
+    public Oval(String title) {
         super(title);
     }
 
-
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
-        this.x = 100;
-        this.y = 100;
-        this.speed = 20.0f;
+        this.x = 0;
+        this.y = 0;
+        this.speed = 10.0f;
     }
 
     @Override
     public void update(GameContainer gameContainer, int delta) throws SlickException {
         this.x += (float)delta/speed;
-        this.y += (float)delta/speed;
+
     }
 
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
-        graphics.drawRect(this.x, this.y, 100, 100);
+        graphics.drawOval(this.x, this.y, 50, 25, 75);
         graphics.drawString("Hello you!", 50, 50);
     }
 
     public static void main(String[] argv) {
         try {
-            AppGameContainer container = new AppGameContainer(new Rectangles("Rectangles"));
+            AppGameContainer container = new AppGameContainer(new Oval("Oval"));
             container.setDisplayMode(800,600,false);
             container.start();
         } catch (SlickException e) {
